a261af8c5a8c94e503cb5f16ffd85b155df3f4c4
emilien.regent@gmail.com <emilien.regent@gmail.com@3f080b48-4a25-e83e-7489-ed8cd96553d4>
2013-01-26 00:31:08 +0000

    Extraction des methodes liés au joueur dans une classe Player

--- js/Game.js
+++ js/Game.js
@@ -10,17 +10,10 @@ function Game()
 	**/
 
 	// Joueur principal
-	this.player = {
-		sprite : null,
-		x : 512 - (48/2),
-		y : 300 - (64/2),
-		w : 48,
-		h : 64,
-		speed : 3
-	};
+	this.player = new Player(this);
 
 	// Cible souris
-	this.target = {
+	this.cible = {
 		sprite : null,
 		x : 512 - (31/2),
 		y : 300 - (31/2),
@@ -40,22 +33,12 @@ function Game()
 		
 		// ====== JOUEUR ======
 		// On stocke une instance unique du joueur dans le tableau qui référence toutes les instances de sprites du jeu
-		this.sprites['img/bob'] = IM.getInstance('img/bob');
-		this.player.sprite = this.sprites['img/bob'];
-		this.player.sprite.animation = new IIG.Animation({
-			sWidth : 48,
-			sHeight : 64,
-			sx : 48,
-			sy : 64 * 2,
-			animDirection : 'left2right',
-			alternate : true,
-			animByFrame : 7
-		});
+		this.player.init();
 
 		// ====== CIBLE ======
 		// On stocke une instance unique de la cible dans le tableau qui référence toutes les instances de sprites du jeu
 		this.sprites['img/target'] = IM.getInstance('img/target');
-		this.target.sprite = this.sprites['img/target'];
+		this.cible.sprite = this.sprites['img/target'];
 
 	};
 	
@@ -69,7 +52,7 @@ function Game()
 		input.listen();
 
 		// On anime le joueur
-		this.animatePlayer();
+		this.player.animate();
 		// On anime la target
 		this.animateTarget();
 
@@ -77,56 +60,15 @@ function Game()
 
 	};
 
-	/**
-	 * Fonction qui s'occupe des déplacements du joueur principal
-	 **/
-	this.animatePlayer = function() {
-
-		var p = this.player;
-
-		// bas
-		if (input.keyboard.down) {
-			if (p.y + p.h + p.speed < HEIGHT) p.y += p.speed;
-			p.sprite.animation.sy = 64 * 2;
-			p.sprite.pauseAnimation = false; // Comme le joueur bouge, on remet l'animation en marche
-		}
-
-		// haut
-		if (input.keyboard.up) {
-			if (p.y - p.speed > 0) p.y -= p.speed;
-			p.sprite.animation.sy = 0;
-			p.sprite.pauseAnimation = false; // Comme le joueur bouge, on remet l'animation en marche
-		}
-
-		// gauche
-		if (input.keyboard.left) {
-			if (p.x - p.speed > 0 ) p.x -= p.speed;
-			p.sprite.animation.sy = 64 * 3;
-			p.sprite.pauseAnimation = false; // Comme le joueur bouge, on remet l'animation en marche
-		}
-
-		// droite
-		if (input.keyboard.right) {
-			if (p.x + p.w + p.speed < WIDTH) p.x += p.speed;
-			p.sprite.animation.sy = 64;
-			p.sprite.pauseAnimation = false; // Comme le joueur bouge, on remet l'animation en marche
-		}
-
-		// Si aucune touche directionnelle n'est activée, la position 'sx' du sprite joueur passe au centre (position arrêt : 48) et on pause l'animation
-		if (!input.keyboard.left && !input.keyboard.up && !input.keyboard.right && !input.keyboard.down) {
-			p.sprite.animation.sx = 48;
-			p.sprite.pauseAnimation = true;
-		}
-
-	};
+	
 
 	/**
 	 * Fonction qui gère la position de la cible via la position souris
 	 **/
 	this.animateTarget = function() {
 
-		this.target.x = input.mouse.x - (this.target.w/2);
-		this.target.y = input.mouse.y - (this.target.h/2);
+		this.cible.x = input.mouse.x - (this.cible.w/2);
+		this.cible.y = input.mouse.y - (this.cible.h/2);
 
 	}
 
@@ -138,26 +80,19 @@ function Game()
 
 		ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-		this.renderPlayer();
+		this.player.render();
 		this.renderTarget();
 
 	};
 
-	/**
-	 * Dessin du joueur
-	 **/
-	this.renderPlayer = function() {
-
-		IM.drawImage(ctx, this.player.sprite, this.player.x, this.player.y);
-
-	};
+	
 
 	/**
 	 * Dessin de la cible (target)
 	 **/
 	this.renderTarget = function() {
 
-		ctx.drawImage(this.target.sprite.data, this.target.x, this.target.y);
+		ctx.drawImage(this.cible.sprite.data, this.cible.x, this.cible.y);
 
 	};
 
---
+++ js/Player.js
@@ -0,0 +1,86 @@
+/**
+ * Player.js
+ **/
+
+function Player(parentObj)
+{
+	this.sprite = null;
+	this.x = 512 - (48/2);
+	this.y = 300 - (64/2);
+	this.w = 48;
+	this.h = 64;
+	this.speed = 3;
+	this.parentObj = parentObj;
+
+	/**
+	 * Initialization
+	 **/
+
+	this.init = function() 
+	{
+		// On stocke une instance unique du joueur dans le tableau qui référence toutes les instances de sprites du jeu
+		this.parentObj.sprites['img/bob'] = IM.getInstance('img/bob');
+		this.sprite = this.parentObj.sprites['img/bob'];
+		this.sprite.animation = new IIG.Animation({
+			sWidth : 48,
+			sHeight : 64,
+			sx : 48,
+			sy : 64 * 2,
+			animDirection : 'left2right',
+			alternate : true,
+			animByFrame : 7
+		});
+	};
+
+	/**
+	 * Fonction qui s'occupe des déplacements du joueur principal
+	 **/
+	this.animate = function() {
+
+		var p = this;
+
+		// bas
+		if (input.keyboard.down) {
+			if (p.y + p.h + p.speed < HEIGHT) p.y += p.speed;
+			p.sprite.animation.sy = 64 * 2;
+			p.sprite.pauseAnimation = false; // Comme le joueur bouge, on remet l'animation en marche
+		}
+
+		// haut
+		if (input.keyboard.up) {
+			if (p.y - p.speed > 0) p.y -= p.speed;
+			p.sprite.animation.sy = 0;
+			p.sprite.pauseAnimation = false; // Comme le joueur bouge, on remet l'animation en marche
+		}
+
+		// gauche
+		if (input.keyboard.left) {
+			if (p.x - p.speed > 0 ) p.x -= p.speed;
+			p.sprite.animation.sy = 64 * 3;
+			p.sprite.pauseAnimation = false; // Comme le joueur bouge, on remet l'animation en marche
+		}
+
+		// droite
+		if (input.keyboard.right) {
+			if (p.x + p.w + p.speed < WIDTH) p.x += p.speed;
+			p.sprite.animation.sy = 64;
+			p.sprite.pauseAnimation = false; // Comme le joueur bouge, on remet l'animation en marche
+		}
+
+		// Si aucune touche directionnelle n'est activée, la position 'sx' du sprite joueur passe au centre (position arrêt : 48) et on pause l'animation
+		if (!input.keyboard.left && !input.keyboard.up && !input.keyboard.right && !input.keyboard.down) {
+			p.sprite.animation.sx = 48;
+			p.sprite.pauseAnimation = true;
+		}
+
+	};
+
+	/**
+	 * Dessin du joueur
+	 **/
+	this.render = function() {
+
+		IM.drawImage(ctx, this.sprite, this.x, this.y);
+
+	};
+}
