8bfc97fff4e71c7161616c8366dc23de0528d8c2
emilien.regent@gmail.com <emilien.regent@gmail.com@3f080b48-4a25-e83e-7489-ed8cd96553d4>
2013-01-26 09:54:11 +0000

    Gestion des points de vie du joueur et déclenchement du game over (log).

--- conf/Game.cfg.js
+++ conf/Game.cfg.js
@@ -4,7 +4,13 @@
 
 var GameConf = 
 {
-	ARENA_SPAWN_PADDING : 50,	/*Coefficient pour décaler le point de spawn des murs*/	
-	ARENA_SPAWN_TIME : 3,		/*Délai de génération des éléments de l'arène*/
-	ARENA_MAX_ENEMY : 10,		/*Nombre maximum d'ennemis*/
+	arena : {
+		SPAWN_PADDING : 50,	/*Coefficient pour décaler le point de spawn des murs*/	
+		SPAWN_TIME : 3,		/*Délai de génération des éléments de l'arène*/
+		MAX_ENEMY : 10,		/*Nombre maximum d'ennemis*/
+	},
+	player : {
+		LIFE : 3,			/*Points de vie du joueur en début de partie*/
+		LIFE_DISPLAY : '<img src="img/life.png"/>',	/*Balise pour afficher une vie du joueur*/
+	}
 }
